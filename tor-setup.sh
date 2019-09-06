2021e6fcc3d025f7bed7ad25d07b99c758077c8e
s2l1 <54559372+s2l1@users.noreply.github.com>
2019-09-06 13:53:05 -0500

    Update tor-setup.sh

--- tor-setup.sh
+++ tor-setup.sh
@@ -26,8 +26,28 @@ apt install tor
 apt install tor-arm
 echo ""
 echo "***"
+echo "Modifying the Tor configuration"
+echo "***"
+echo ""
+sed -i '56d' /etc/tor/torrc
+sed -i "56i ControlPort 9051" /etc/tor/torrc
+sed -i '60d' /etc/tor/torrc
+sed -i "60i CookieAuthentication 1" /etc/tor/torrc
+sed -i "61i CookieAuthFileGroupReadable 1" /etc/tor/torrc
+# method used with the sed command is to delete entire lines 56, 60 and add new line without a comment marker
+# double check /etc/tor/torrc
+echo ""
+echo "***"
+echo "Restarting Tor"
+echo "***"
+echo ""
+systemctl restart tor
+sleep 5s
+echo ""
+echo "***"
 echo "Check the version of Tor and that the service is up and running"
 echo "***"
 echo ""
 tor --version
+sleep 5s
 systemctl status tor
