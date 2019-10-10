4654624bb31ad49a626d1b4e2818f90c079568e3
s2l1 <54559372+s2l1@users.noreply.github.com>
2019-10-09 21:43:13 -0500

    Update Default_Dojo_Setup_Script01.sh

--- Default_Dojo_Setup_Script01.sh
+++ Default_Dojo_Setup_Script01.sh
@@ -140,4 +140,121 @@ echo ""
 sleep 20s
 # ufw setup ends
 
+# pip setup starts
+echo ""
+echo "***"
+echo "Installing the Python Package Installer and its dependencies"
+echo "***"
+echo ""
+sleep 5s
+cd ~
+apt-get install python3-dev 
+apt-get install libffi-dev 
+apt-get install libssl-dev 
+apt-get install build-essential
+# these are useful libs in general for your system
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# this is a convenient script, be sure to check it before running!!!
+python3 get-pip.py
+# pip setup ends
+
+# docker setup starts
+echo ""
+echo "***"
+echo "Installing docker and docker-compose"
+echo "***"
+echo ""
+
+echo ""
+echo "***"
+echo "Downloading docker install script"
+echo "Check git commit on line 19 https://github.com/docker/docker-install/blob/master/install.sh before executing"
+echo "***"
+echo ""
+cd ~
+curl -fsSL https://get.docker.com -o get-docker.sh
+# check git commit on line 19 here https://github.com/docker/docker-install/blob/master/install.sh
+
+echo ""
+echo "***"
+echo "Make sure to verify the contents of the script just you downloaded!!!"
+echo "Open a new window now to verify the authenticity of the scripts"
+echo "Enter Y if ready to proceed after verfying scripts, any other key to exit"
+echo "***"
+echo ""
+read input
+if [ $input != "Y" -o $input != "y" ]; then
+
+echo ""
+echo "***"
+echo "Exiting now"
+echo "***"
+echo ""
+fi
+
+if [ $input == "Y" -o $input == "y" ]; then
+echo ""
+echo "***"
+echo "Proceeding with install"
+echo "***"
+echo ""
+
+echo ""
+echo "***"
+echo "Installing docker and docker-compose"
+echo "***"
+echo ""
+sh get-docker.sh
+sleep 5s
+python3 -m pip install --upgrade docker-compose
+sleep 5s
+
+echo ""
+echo "***"
+echo "Checking docker version"
+echo "***"
+echo ""
+docker -v
+sleep 5s
+
+echo ""
+echo "***"
+echo "Take a look at what PIP has installed on your system"
+echo "***"
+echo ""
+python3 -m pip list
+sleep 10s
+
+echo ""
+echo "***"
+echo "Now to configuring docker to use the external SSD"
+echo "***"
+echo ""
+echo "{ 
+                  "data-root": "/mnt/usb/docker" 
+}" > /etc/docker/daemon.json
+
+echo ""
+echo "***"
+echo "Restarting docker"
+echo "***"
+echo ""
+systemctl daemon-reload
+systemctl start docker
+
+echo ""
+echo "***"
+echo "Check that docker is using the SSD"
+echo "***"
+echo ""
+docker info | grep "Docker Root Dir:"
+sleep 5s
+
+echo ""
+echo "***"
+echo "Try rebooting if you do not see your SSD listed"
+echo "***"
+echo ""
+fi
+# docker setup ends
 
